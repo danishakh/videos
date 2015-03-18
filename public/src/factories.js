aa8abaa203e7d1cc18872b4f42dee249a75b1f42
danishakh <danishakh@gmail.com>
2015-03-18 16:56:11 -0700

    Update factories.js

--- public/src/factories.js
+++ public/src/factories.js
@@ -1,4 +1,6 @@
+//create our resource as a factory
+
 angular.module('videosApp')
 	.factory('Video', function ($resource) {
-		return $resource('/api/video/:id', { id : '@id'})
+		return $resource('/api/video/:id', { id : '@id'});	//'api/video/:id' links to the id of each of our resource instances 
 	});
