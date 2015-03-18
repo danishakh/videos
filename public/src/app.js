7852a0c6528cc0cdd0525bb3c6caf896a098ef81
danishakh <danishakh@gmail.com>
2015-03-18 16:55:04 -0700

    Update app.js

--- public/src/app.js
+++ public/src/app.js
@@ -1,10 +1,12 @@
-angular.module('videosApp', ['ngRoute', 'ngResource'])
+//create/configure our angular application
+
+angular.module('videosApp', ['ngRoute', 'ngResource'])			//added ngRoute and ngResource dependecies to the application
 	.config(function ($routeProvider, $locationProvider){
 		$routeProvider
-			.when('/videos', {
+			.when('/videos', {				//when route matches '/videos', call controller and render template
 				controller: 'ListController',
 				templateUrl: 'views/list.html'
 			});
-		$locationProvider.html5Mode(true);
+		$locationProvider.html5Mode(true);			//enable this because we don't want to use the default '/#!/videos'
 	});
 
