56d0f051c467f2eaa6ac5a8fb3637aa5c2cc1027
danishakh <danishakh@gmail.com>
2015-03-18 16:54:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+//server.js file to startup the localhost server for our app
+
 var express = require('express'),
 	api		= require('./api'),
     app     = express();
 
 app
-    .use(express.static('./public'))
+    .use(express.static('./public'))		//statically load all files in public directory
     .use('/api', api)
     .get('*', function (req, res) {
         res.sendfile('public/main.html');
